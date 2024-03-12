57d20a96eb12cb20946af8ff75e294bea15f379f
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-12 23:14:35 +0000

    fix(i18n): use languages list from app.config

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(["fr", "en"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")

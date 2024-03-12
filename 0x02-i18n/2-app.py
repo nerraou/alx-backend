7a22dfbc0aca305494f26de9685a08eac42f5fc7
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-12 23:18:45 +0000

    chore(i18n): use the appropriate template

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route("/")
 def index():
     """home page"""
-    return render_template("0-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":

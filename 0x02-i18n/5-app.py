a183912493e41695e31615fbb583d16903f35ee8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-13 01:01:23 +0000

    feat: login mock

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 """ Flask Application """
 from os import environ
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """get user by id"""
+    try:
+        login_as = request.args.get("login_as")
+        if login_as is None:
+            return None
+        return users.get(int(login_as), None)
+    except e:
+        return None
 
 
 class Config:
@@ -24,10 +42,17 @@ def get_locale():
     return request.args.get("locale", "en")
 
 
+@app.before_request
+def before_request():
+    """before request handler"""
+    print(get_user())
+    g.user = get_user()
+
+
 @app.route("/")
 def index():
     """home page"""
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":

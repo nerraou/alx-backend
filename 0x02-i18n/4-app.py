30136777eb7af0ddec0b4799e4e610075f65ac90
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-13 00:45:45 +0000

    feat: get locale from query_string

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Flask Application """
+from os import environ
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """app config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """locale selector method"""
+    # request.query_string.
+    return request.args.get("locale", "en")
+
+
+@app.route("/")
+def index():
+    """home page"""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)

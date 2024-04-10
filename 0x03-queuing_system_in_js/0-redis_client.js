f5b04092cbdeae910d03f612686aff9ce8bf88bd
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-04-10 22:08:14 +0000

    fix: 0-redis_client error message format

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,13 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient(1);
 
 client.on('ready', () => {
   console.log('Redis client connected to the server');
 });
 
 client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error.message}`);
+  console.log(
+    `Redis client not connected to the server: Error: ${error.message}`,
+  );
 });
